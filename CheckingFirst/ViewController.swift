4d947f6072657431302b095023a7e714cefa17e5
Ariful0007 <arifulpub143@gmail.com>
2023-05-02 09:02:05 +0600

    strtf

--- CheckingFirst/ViewController.swift
+++ CheckingFirst/ViewController.swift
@@ -8,10 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    
+    @IBOutlet weak var TestView: UILabel!
+    var click = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        TestView.text="My Bluetooth Testing"
+        TestView.font = .italicSystemFont(ofSize: 12)
+        // TestView.backgroundColor=UIColor.white
+        TestView.textAlignment = .center
+        TestView.textColor  = UIColor.white
+        TestView.shadowColor = UIColor.blue
+        TestView.isHighlighted = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTextTap(sender:)))
+        TestView.isUserInteractionEnabled = true
+        TestView.addGestureRecognizer(tap)
+        
+        
+        
+    }
+    let flag = 1;
+    @objc func didTextTap(sender : UITapGestureRecognizer)
+    {
+        if(flag==1)
+        {
+            TestView.textColor = UIColor.red
+        }
+        
     }
 
 
